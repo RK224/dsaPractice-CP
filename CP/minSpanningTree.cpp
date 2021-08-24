3a279793a702d8d4641eb8d6ab274939d0e48fa9
rk224 <rahulkhapre1998@gmail.com>
2021-08-25 02:03:06 +0530

    Implemented Prims algorithm for MST

--- CP/minSpanningTree.cpp
+++ CP/minSpanningTree.cpp
@@ -91,6 +91,32 @@ void printSpanningTree(vector<tuple<int,int,int>> spanningTree){
 	cout << "Total weight " << totalWeight << endl;
 }
 
+void addNodeToMst(int v, int exclude, vector<pair<int,int>> adjNodes, vector<bool> &inMst, priority_queue<tuple<int,int,int>> &pq){
+	inMst[v] = true;
+	int u,w;
+	for(pair<int,int> edge : adjNodes){
+		u = edge.first; w = edge.second;
+		if(u != exclude) pq.push({-1*w, v, u});
+	}
+}
+
+vector<tuple<int,int,int>> Prim(vector<vector<pair<int,int>>> adjList, int n){
+	vector<tuple<int,int,int>> mst;
+	priority_queue<tuple<int,int,int>> pq;
+	vector<bool> inMst(n, false);
+	addNodeToMst(0, -1, adjList[0], inMst, pq);
+	int u,v,w;
+	while(!pq.empty()){
+		tuple<int,int,int> edge = pq.top(); pq.pop();
+		w = -1* get<0>(edge); v = get<1>(edge); u = get<2>(edge);
+		if(!inMst[u]){
+			mst.push_back({v,u,w});
+			addNodeToMst(u, v, adjList[u], inMst, pq);
+		}
+	}
+	return mst;
+}
+
 int main(int argc, char* argv[]){
 	vector<tuple<int,int,int>> wEdges;
 	char* filename;
@@ -111,4 +137,13 @@ int main(int argc, char* argv[]){
 	printUnionFind(pUF);
 	cout << "isSameComponent 0 and 1 : " << isSameComponent(0, 1, pUF) << endl;
 	printSpanningTree(Kruskal(wEdges, n));
+
+	vector<vector<pair<int,int>>> adjList(n, vector<pair<int,int>>());
+	for(tuple<int,int,int> edge : wEdges){
+		int a,b,w;
+		a = get<0>(edge); b = get<1>(edge); w = get<2>(edge);
+		adjList[a].push_back({b,w});
+		adjList[b].push_back({a,w});
+	}
+	printSpanningTree(Prim(adjList,n));
 }
